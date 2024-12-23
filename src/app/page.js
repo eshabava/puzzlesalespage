2158c0af7a8d2f1ed2c57e965b8572a48f869127
eshabava <bankonrekhajain@gmail.com>
2024-12-23 14:16:43 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,12 @@ import ComingSoon from '@/components/home/ComingSoon'
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+      {/* Hidden WarriorPlus button for approval */}
+      <div className="hidden">
+        <a href="https://warriorplus.com/o2/buy/xzp148/k2fb2h/zmnfrg">
+          <img src="https://warriorplus.com/o2/btn/fn100011001/xzp148/k2fb2h/418924" alt="Buy Now" />
+        </a>
+      </div>
       <Hero />
       <Features />
       <HowItWorks />
