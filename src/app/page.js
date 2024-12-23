9d8c9d2aaa2072030347dad629546f1bcb8f6281
eshabava <bankonrekhajain@gmail.com>
2024-12-23 17:08:38 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Hero from '@/components/home/Hero'
 import Features from '@/components/home/Features'
 import HowItWorks from '@/components/home/HowItWorks'
 import Pricing from '@/components/home/Pricing'
-import Testimonials from '@/components/home/Testimonials'
 import FAQ from '@/components/home/FAQ'
 import Founders from '@/components/Founders'
 import FinalCTA from '@/components/home/FinalCTA'
@@ -31,7 +30,6 @@ export default function Home() {
         <Features />
         <HowItWorks />
         <Pricing />
-        <Testimonials />
         <Founders />
         <FAQ />
         <FinalCTA />
